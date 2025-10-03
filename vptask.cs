6510972284bbd77b9f0b545bd0507e57f25c498f
mahnooram123 <mahnoormustafa640@gmail.com>
2025-10-03 10:21:15 +0500

    Add files via upload
    this is my first vp task of printing hello world

---
+++ vptask.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("hello world");
+    }
+}
