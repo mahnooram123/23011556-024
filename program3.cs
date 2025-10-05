3ee8a0f4c1e633b487817b5a1bd3c5c8ec32fa26
mahnooram123 <mahnoormustafa640@gmail.com>
2025-10-05 18:42:22 +0500

    Add files via upload

--- program3.cs
+++ program3.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace mahnoorconsoleapp
+class GradeCalculator
 {
-    internal class Class1
+    static void Main()
     {
+        Console.WriteLine(" Calculator");
+        Console.Write("Enter your marks (0-100): ");
+        int marks = Convert.ToInt32(Console.ReadLine());
+        if (marks < 0 || marks > 100)
+        {
+            Console.WriteLine("Invalid marks! Please enter a number between 0 and 100.");
+        }
+        else if (marks >= 90)
+        {
+            Console.WriteLine("Grade: A+");
+        }
+        else if (marks >= 80)
+        {
+            Console.WriteLine("Grade: A");
+        }
+        else if (marks >= 70)
+        {
+            Console.WriteLine("Grade: B");
+        }
+        else if (marks >= 60)
+        {
+            Console.WriteLine("Grade: C");
+        }
+        else if (marks >= 50)
+        {
+            Console.WriteLine("Grade: D");
+        }
+        else
+        {
+            Console.WriteLine("Grade: F");
+        }
     }
 }
