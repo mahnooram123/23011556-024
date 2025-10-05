31092f80b20b28559b3f4eeca0239f4860e653ed
mahnooram123 <mahnoormustafa640@gmail.com>
2025-10-05 18:59:54 +0500

    Add files via upload

--- program10.cs
+++ program10.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace mahnoorconsoleapp
+class NumberInArray
 {
-    internal class Class1
+    static void Main()
     {
+        int[] numbers = new int[5]; 
+
+        Console.WriteLine("Enter 5 numbers:");
+        for (int i = 0; i < numbers.Length; i++)
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter a number to check: ");
+        int target = Convert.ToInt32(Console.ReadLine());
+
+        bool exists = Array.Exists(numbers, n => n == target);
+
+        Console.WriteLine(exists
+            ? $"{target} exists in the array."
+            : $"{target} does not exist in the array.");
     }
 }
