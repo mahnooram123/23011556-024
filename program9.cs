9a4be7a0fbb133450e43d46229b224518f1d8dbf
mahnooram123 <mahnoormustafa640@gmail.com>
2025-10-05 18:57:42 +0500

    Add files via upload

--- program9.cs
+++ program9.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace mahnoorconsoleapp
+class EvenOddCounter
 {
-    internal class Class1
+    static void Main()
     {
+        int[] numbers = new int[10];
+        int evenCount = 0, oddCount = 0;
+        Console.WriteLine("Enter 10 integers:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Number {i + 1}: ");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+            if (numbers[i] % 2 == 0)
+                evenCount++;
+            else
+                oddCount++;
+        }
+
+        Console.WriteLine($"\nNumber of even numbers: {evenCount}");
+        Console.WriteLine($"Number of odd numbers: {oddCount}");
     }
 }
