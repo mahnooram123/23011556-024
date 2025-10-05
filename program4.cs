f6896f832e9cce3ea3c607031e8675cc45dca53e
mahnooram123 <mahnoormustafa640@gmail.com>
2025-10-05 18:46:53 +0500

    Add files via upload

--- program4.cs
+++ program4.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace mahnoorconsoleapp
+class SumCalculator
 {
-    internal class Class1
+    static void Main()
     {
+        Console.Write("Enter a number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;  // Add i to sum
+        }
+
+        Console.WriteLine("The sum of numbers from 1 to {n} is {sum}");
     }
 }
