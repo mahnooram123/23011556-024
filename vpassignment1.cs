84164fdc82107f058b17d30485d83bb19ff5b0fd
mahnooram123 <mahnoormustafa640@gmail.com>
2025-10-05 10:30:26 +0500

    Add files via upload

---
+++ vpassignment1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mahnoorconsoleapp
+{
+      internal class program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Name: Mahnoor Mustafa Roll No.: 23011556-024 ");
+            Console.WriteLine("Table of 2");
+
+            for (int i=1;i<=10; i++)
+            {
+                Console.WriteLine("2 * " + i + "=" + (2 * i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
