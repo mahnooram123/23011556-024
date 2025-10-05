b405a145d282f532f22c57b9e68780ae539b872a
mahnooram123 <mahnoormustafa640@gmail.com>
2025-10-05 18:52:13 +0500

    Add files via upload

--- program6.cs
+++ program6.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace mahnoorconsoleapp
+class FactorialCalculator
 {
-    internal class Class1
+    static void Main()
     {
+        Console.Write("Enter a number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int factorial = 1;
+        int i = 1;
+
+        while (i <= n)
+        {
+            factorial *= i;  // Multiply factorial by i
+            i++;
+        }
+
+        Console.WriteLine($"Factorial of {n} is {factorial}");
     }
 }
