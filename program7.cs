e42feb9659e9e101cec2607ab73492eb7db7b854
mahnooram123 <mahnoormustafa640@gmail.com>
2025-10-05 18:53:40 +0500

    Add files via upload

--- program7.cs
+++ program7.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace mahnoorconsoleapp
+class ReverseNumber
 {
-    internal class Class1
+    static void Main()
     {
+        Console.Write("Enter an integer: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        int reversed = 0;
+        int original = num;
+
+        while (num != 0)
+        {
+            int digit = num % 10;      // Get the last digit
+            reversed = reversed * 10 + digit; // Append it to reversed number
+            num /= 10;                 // Remove the last digit
+        }
+
+        Console.WriteLine($"The reverse of {original} is {reversed}");
     }
 }
