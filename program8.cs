c114153d540d1f9049604a893a7f922d251a0504
mahnooram123 <mahnoormustafa640@gmail.com>
2025-10-05 18:55:57 +0500

    Add files via upload

--- program8.cs
+++ program8.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace mahnoorconsoleapp
+class ArrayMinMax
 {
-    internal class Class1
+    static void Main()
     {
+        int[] numbers = new int[10];
+        Console.WriteLine("Enter 10 integers:");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Number {i + 1}: ");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int max = numbers[0];
+        int min = numbers[0];
+        for (int i = 1; i < 10; i++)
+        {
+            if (numbers[i] > max)
+                max = numbers[i];
+            if (numbers[i] < min)
+                min = numbers[i];
+        }
+
+        Console.WriteLine($"\nMaximum element: {max}");
+        Console.WriteLine($"Minimum element: {min}");
     }
 }
